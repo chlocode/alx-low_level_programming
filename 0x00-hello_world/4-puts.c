e7c5b61bb5acc38097cf5ec54d237f1599ec7394
chlocode <chalafeghaquakumey@gmail.com>
2023-03-16 11:56:41 -0700

    fixed the function documentationcomment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 
-/*prints text*/
+/**
+ * prints text with puts() function*/
 
 int main(void)
 {
 	char Thestr[] = "Programming is like building a multilingual puzzle";
+	
 	puts(Thestr);
 
 	return (0);

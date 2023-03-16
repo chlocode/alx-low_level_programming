dcf000928a848929055035f7b2538d856a88c9dd
chlocode <chalafeghaquakumey@gmail.com>
2023-03-16 14:35:29 -0700

    attempted to write a c program without errors

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: 0 if excecution is successful
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome isa piece of art,");
+
+	return (0);
+}

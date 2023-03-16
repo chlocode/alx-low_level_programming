aeb40b249b02eea5af135c9c796404397baef520
chlocode <chalafeghaquakumey@gmail.com>
2023-03-16 12:04:17 -0700

    trailing spaces again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 
 /**
- * prints text with puts() function*/
+ * prints text with puts() function
+ */
 
 int main(void)
 {
 	char Thestr[] = "Programming is like building a multilingual puzzle";
-	
+
 	puts(Thestr);
 
 	return (0);

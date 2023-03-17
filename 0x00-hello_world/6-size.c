2bd3459650ed7e8fb67cf9b43aba338b515eb1d0
chlocode <chalafeghaquakumey@gmail.com>
2023-03-16 18:23:39 -0700

    Would i be able to run this sizeof() program without errors?

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: 0 if execution is successful
+ */
+int main(void)
+{
+	printf("%lu\n", sizeof(int));
+
+	printf("%lu\n", sizeof(float));
+
+	printf("%lu\n", sizeof(double));
+
+	printf("%lu", sizeof(char));
+
+	return (0);
+}

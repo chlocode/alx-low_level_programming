1fb967fd74659ae1211289dc672ae7bc57899dd3
chlocode <chalafeghaquakumey@gmail.com>
2023-03-18 10:30:48 -0700

    added next line with putchar and put the arg void in main

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,13 +6,14 @@
  * in lowercase followed by a new line
  * Return: 0 if execution is successful
  */
-int main()
+int main(void)
 {
 	char letter;
 
-	for(letter = 'a' ; letter <= 'z' ; letter++)
+	for (letter = 'a' ; letter <= 'z' ; letter++)
 	{
 		putchar(letter);
+		putchar('\n');
 	}
 	return (0);
 }

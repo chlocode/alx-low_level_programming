36bd35dfeaf2e8caed0fc84f6553a82ddf89c07a
chlocode <chalafeghaquakumey@gmail.com>
2023-03-18 12:40:47 -0700

    used a for loop condition

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This program prints all single
+ * digit numbers of base ten starting from 0
+ * followed by a new line
+ * Return: 0 if execution is successful
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}

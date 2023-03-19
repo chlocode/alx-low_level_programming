034cbc1c63b03bdaa47b72f8cf935263e73a5105
chlocode <chalafeghaquakumey@gmail.com>
2023-03-19 16:25:31 -0700

    decreased the variable letter

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This program prints the alphabet
+ * in lowercase and reversed
+ * followed by a new line
+ * Return: 0 if execution is successful
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a' ; letter <= 'z' ; letter--)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}

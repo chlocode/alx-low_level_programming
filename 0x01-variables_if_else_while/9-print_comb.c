605000127581a93279c45a8e2e3894b3ad6a147b
chlocode <chalafeghaquakumey@gmail.com>
2023-03-19 18:46:21 -0700

    added an if statement allowing the comma and space to be looped till number 8

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This program prints all single digit numbers
+ * separated by space and comma
+ * Return: 0 if execution is successful
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar((char)(i));
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+

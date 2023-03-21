54c8ff3206a3cf41af514b9416805ad7571a9997
chlocode <chalafeghaquakumey@gmail.com>
2023-03-21 11:54:19 -0700

    made a new directory and added a file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This program prints "_putchar"
+ * Return: 0 if execution is successful
+ */
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}

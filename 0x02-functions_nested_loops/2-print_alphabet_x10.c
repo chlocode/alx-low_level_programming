fc88a4579fea2f00be37d1fa163c39f326618f92
chlocode <chalafeghaquakumey@gmail.com>
2023-03-22 04:05:33 -0700

    nested the print_alphabet loo in a while loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ * Return: nothing
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int set = 0;
+
+	while (set < 10)
+	{
+		for (letter = 'a' ; letter <= 'z' ; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+		set++;
+	}
+	return;
+}

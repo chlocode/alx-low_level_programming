7b3abc98769799828f1d036d055998497e82ce1f
chlocode <chalafeghaquakumey@gmail.com>
2023-03-22 15:38:46 -0700

    created another variable and used %10

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ * @v: number to be computed
+ *
+ * Return: last digit of the computed number
+ */
+int print_last_digit(int v)
+{
+	int Ld = v % 10;
+	
+	return (Ld);
+}

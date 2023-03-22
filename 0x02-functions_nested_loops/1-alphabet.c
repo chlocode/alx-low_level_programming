e90feb5de1978384b96749c2d81c48d8ee4af088
chlocode <chalafeghaquakumey@gmail.com>
2023-03-21 23:55:12 -0700

    added test file,fixed next line, used print_alphabet function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 #include "main.h"
-#include "_putchar.c"
+
 /**
- * main - check the code
+ * main - Entry point
  *
  * Description: This program prints the alphabet
  * in lowercase followed by a new line.
- * Return: 0 as it's void
+ * Return: 0 if execution is successful
  */
-int main(void)
+void print_alphabet(void)
 {
 	char letter;
 	for (letter = 'a' ; letter <= 'z' ; letter++)
 	{
 		_putchar(letter);
-		_putchar('\n');
 	}
-	return (0);
+	_putchar('\n');
 }

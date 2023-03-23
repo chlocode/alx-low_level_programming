4eb8dcf78544d97e13523c3e275a01ec00fd9bb4
chlocode <chalafeghaquakumey@gmail.com>
2023-03-23 13:47:30 -0700

    made a new directory, added README file, main.h file and program file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ *
+ * Return: 1 if uppercase else 0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}

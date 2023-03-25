5cc0f8dab8af3659a20db94123469317175abecc
chlocode <chalafeghaquakumey@gmail.com>
2023-03-24 17:58:57 -0700

    looped the space character

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,13 +8,16 @@
 void print_diagonal(int n)
 {
 	int slash;
+	int space;
 
 	for (slash = 0 ; slash < n ; slash++)
 	{
+		for (space = 0 ; space < slash ; space++)
+			_putchar(' ');
 		_putchar('\\');
 		_putchar('\n');
 		if (n <= 0)
 			_putchar('\n');
 	}
-	_putchar('\n');
+	/*_putchar('\n');*/
 }

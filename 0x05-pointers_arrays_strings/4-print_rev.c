92017b72a1ebd7c4f70bc0b676d70bf269ec83f1
chlocode <chalafeghaquakumey@gmail.com>
2023-03-29 06:50:56 -0700

    added another while loop to decrease i

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,10 +7,11 @@
  */
 void print_rev(char *s)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0 ; s[i] ; i++)
-		while (i--)
-			_putchar(s[i]);
+	while (s[i])
+		i++;
+	while(i--)
+		_putchar(s[i]);
 	_putchar('\n');
 }

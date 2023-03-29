fe03e8c6dd67d8d9fbbdbffe6a7c42cd37bf6a87
chlocode <chalafeghaquakumey@gmail.com>
2023-03-29 07:58:49 -0700

    not sure what i did here...

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i = *s - 1;
+
+	while (i >= 0)
+	{
+		i++;
+	}
+}

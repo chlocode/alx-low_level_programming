c67e5860371a3c7fbbef56345fed190c6ab99628
chlocode <chalafeghaquakumey@gmail.com>
2023-03-30 09:30:40 -0700

    iterated through src and added to dest

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_strcat - concatenates 2 strings
+*@dest: string 1
+*@src: string 2
+*
+*Return: concatenated string
+*/
+char *_strcat(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0 ; i >= 0 && src[i] != '\0' ; i++)
+		dest[*dest + i] = src[i];
+	dest[*dest + i] = '\0';
+	return (dest);
+}

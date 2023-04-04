c5156f08afeb711dff5d9a0383dc34fa8be57d98
chlocode <chalafeghaquakumey@gmail.com>
2023-04-03 18:35:50 -0700

    fixed betty error

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,7 +2,6 @@
 /**
 *_memset - fills memory with a constant byte
 *@s: pointer to the block of memory to be filled
-*@b: the value to fill s with
 *@n: the number of bytes to fill
 *
 *Return: s
@@ -10,6 +9,7 @@
 char *_memset(char *s, char b unsigned int n)
 {
 	int num;
+
 	for (num = 0 ; num < n ; num++)
 	{
 		s[num] = b;

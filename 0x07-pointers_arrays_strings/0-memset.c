9e6968ae9fa0bc0311e744d18037b556ddad13bf
chlocode <chalafeghaquakumey@gmail.com>
2023-04-03 18:43:01 -0700

    added comma between function parameters

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,13 +2,14 @@
 /**
 *_memset - fills memory with a constant byte
 *@s: pointer to the block of memory to be filled
+*@b: the value to fill s with
 *@n: the number of bytes to fill
 *
 *Return: s
 */
-char *_memset(char *s, char b unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	int num;
+	unsigned int num;
 
 	for (num = 0 ; num < n ; num++)
 	{

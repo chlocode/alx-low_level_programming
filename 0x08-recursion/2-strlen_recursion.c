e651b3d34d40d3492df34896f0a71147fb9c075e
chlocode <chalafeghaquakumey@gmail.com>
2023-04-05 13:50:12 -0700

    returned -1 for n<0, returned 1 for n==0

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,16 +7,14 @@
 */
 int _strlen_recursion(char *s)
 {
+
 	int length = 0;
 
-	while (*s)
+	if (*s == '\0')
+		return (0);
+	else
 	{
-		if (*s !=  0)
-		{
-			length++;
-			_strlen_recursion(s + 1);
-		}
-		s++;
+		length += _strlen_recursion(s + 1);
 	}
-	return (length++);
+	return (length);
 }

408a7360be807ae9a301a274812705b6ca1cc241
chlocode <chalafeghaquakumey@gmail.com>
2023-04-06 17:33:37 -0700

    multiplied x by itself and decreased y till base condition y<0 was met.

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,8 +10,7 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	else
-	{
-		return _pow_recursion(x ** y);
-	}
+	if (y == 0)
+		return (1);
+	return x * _pow_recursion(x, (y - 1));
 }

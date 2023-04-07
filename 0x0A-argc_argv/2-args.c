5285911b356d49820299b44e2fd823084a14d187
chlocode <chalafeghaquakumey@gmail.com>
2023-04-07 09:12:13 -0700

    Modified the 0-whatsmyname.c code to print all args instead of just the firstone

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main -Entry point
+*@argc: argument counter
+*@argv: argument array
+*
+*Return: 0 if execution is successful
+*/
+int main(int argc, char *argv[])
+{
+	int count;
+	
+	for (count = 0 ; count < argc ; count++)
+	{
+		printf("%s\n", argv[count]);
+	}
+	return (0);
+}

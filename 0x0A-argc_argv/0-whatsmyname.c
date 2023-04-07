58ec314063b3dac8e3638c34ee89fe854413a59b
chlocode <chalafeghaquakumey@gmail.com>
2023-04-07 07:27:56 -0700

    fixed betty error on lines 9, 12 and 17

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,13 +6,13 @@
 *
 *Return: 0 if execution is successful
 */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int count;
-	
+
 	for (count = 0 ; count < argc ; count++)
 	{
 		printf("%s\n", argv[0]);
 	}
-	return 0;
+	return (0);
 }

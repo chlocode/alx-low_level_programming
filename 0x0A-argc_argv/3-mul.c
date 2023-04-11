77babd8fba379f30bc705d2f67d6f6686239080a
chlocode <chalafeghaquakumey@gmail.com>
2023-04-10 21:33:04 -0700

    declared variables before initializing them

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,15 +9,18 @@
 */
 int main(int argc, char **argv)
 {
-	int a = atoi(argv[1]);
-	int b = atoi(argv[2]);
+	int a;
+	int b;
 	int result;
-	
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
 	result = a * b;
 	printf("%d\n", result);
 	return (0);

fdaf423a33d7f7366523a028a0979728e7d40be0
chlocode <chalafeghaquakumey@gmail.com>
2023-04-12 22:08:54 -0700

    fixed bad line in 0-create_array.c documentation'

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 /**
 *create_array - creates an array of chars
-and initializes it with a specific char
+*and initializes it with a specific char
 *@size: size of array
 *@c: specific char
 *

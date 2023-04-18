8a64b18cfa14b03e6cd496d17fc08f9bc2dbb885
chlocode <chalafeghaquakumey@gmail.com>
2023-04-18 05:29:19 -0700

    added betty style documentation

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_HEADER
 #define DOG_HEADER
-
+/**
+ * struct dog - contains ID info for dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: name and owner are pointers.
+ */
 struct dog
 {
 	char *name;

155136996675e8475517d338051129195b6ddc75
chlocode <chalafeghaquakumey@gmail.com>
2023-04-18 07:47:16 -0700

    used if-else if-else instead. Makes it neater

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,15 +11,30 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	if (d->name == NULL)
+	if (d->name == NULL && d->owner == NULL)
+	{
 		printf("Name: (nil)\n");
-	else
-		printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	if (d->owner == NULL)
+		printf("Age: %f\n", d->age);
 		printf("(nil)\n");
-	else
+	}
+	else if (d->name == NULL)
+	{
+		printf("Name: (nil)\n");
+		printf("Age: %f\n", d->age);
 		printf("Owner: %s\n", d->owner);
+	}
+	else if (d->owner == NULL)
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("(nil)\n");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
 	/**
 	*printf("Name: %s\n", d->name);
 	*printf("Age: %d\n", d->age);

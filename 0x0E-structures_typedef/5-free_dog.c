34eb2268aadd9410dab429e257f122995a6eda98
chlocode <chalafeghaquakumey@gmail.com>
2023-04-18 15:20:26 -0700

    freed the dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_dog - frees memory allocation
+ * @d: pointer to struct
+ * 
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free (d);
+	free((*d).name);
+	free((*d).owner);
+}

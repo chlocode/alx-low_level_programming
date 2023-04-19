41459d3f7064fce08114fc1f1f30ef9cf4650786
chlocode <chalafeghaquakumey@gmail.com>
2023-04-18 17:12:01 -0700

    fixed betty errors and code fails

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
-	printf("%s/n", __FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
